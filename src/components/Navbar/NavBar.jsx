9da89aed917b4ed1ddcf85db7162f78fbceedcd5
damien <damien.changenot@gmail.com>
2024-01-19 16:55:44 +0100

    update color navbar + footer

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -27,8 +27,8 @@ export default function NavBar()
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 z-50">
-                    <li><Link to="/">Acceuil</Link></li>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 z-50">
+                    <li><Link to="/" >Acceuil</Link></li>
                     <li>
                         <a>Métiers</a>
                         <ul className='p-2'>
@@ -42,27 +42,35 @@ export default function NavBar()
                     <li><Link to="partenaires">Mécénats / Partenariats</Link></li>
                 </ul>
                 </div>
-                <img src={logoHeader} alt="" width="70"/>
+                <img src={logoHeader} className='shadow-lg' alt="" width="70"/>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <div className="menu menu-horizontal px-1">
-                <Link className='mx-3' to="/">Acceuil</Link>
-            
-                <div className="dropdown dropdown-hover">
-                    <label tabIndex={0} className='mx-3'>Métiers</label>
-                    <ul className='p-2 z-50 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52'>
-                        <li><Link to="/services">Services</Link></li>
-                        <li><Link to="/ingenierie">Ingéniérie</Link></li>
-                        <li><Link to="/solutions">Solutions</Link></li>
-                    </ul>
-                </div>
-         
+                <ul className="menu menu-horizontal px-1 bg-base-100 rounded-lg shadow-lg">
+                    <li><Link className='mx-3' to="/">Acceuil</Link></li>
                 
-                <Link className='mx-3' to="/recrutement">Recrutement</Link>
-                <Link className='mx-3' to="/contact">Contact</Link>
-                <Link className='mx-3' to="/partenaires">Mécénats / Partenariats</Link>
-                <a href="https://get.teamviewer.com/q7c2du5" target="_blank">Support Technique</a>
-                </div>
+                    <li tabIndex={0} className="dropdown dropdown-hover">
+                        <summary  className='mx-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+						        <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"></path>
+					        </svg>
+                            Métiers
+                        </summary>
+                        <ul className='p-2 z-50 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52'>
+                            <li><Link to="/services">Services</Link></li>
+                            <li><Link to="/ingenierie">Ingéniérie</Link></li>
+                            <li><Link to="/solutions">Solutions</Link></li>
+                        </ul>
+                    </li>
+            
+                    
+                    <li><Link className='mx-3 ' to="/recrutement">
+                            Recrutement
+                        </Link>
+                    </li>
+                    <li><Link className='mx-3' to="/contact">Contact</Link></li>
+                    <li><Link className='mx-3' to="/partenaires">Mécénats / Partenariats</Link></li>
+                    <li><a className='mx-3' href="https://get.teamviewer.com/q7c2du5" target="_blank">Support Technique</a></li>
+                </ul>
             </div>
             <div className="navbar-end ">
             <label className="swap swap-rotate mr-10"> 
