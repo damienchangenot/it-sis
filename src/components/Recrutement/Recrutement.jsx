2c25bd9e837531e6983d09b412424d7c4ba45837
Changenot Damien <damien.changenot@gmail.com>
2024-01-21 23:32:03 +0100

    add icon navbar

--- src/components/Recrutement/Recrutement.jsx
+++ src/components/Recrutement/Recrutement.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import hire from '../../assets/hire.jpg'
 
 export default function Recrutement() {
   return (
-    <div>Recrutement</div>
+    <section>
+        <div className="hero min-h-[80vh]" style={{backgroundImage: `url(${hire})`}}>
+          <div className="hero-overlay bg-opacity-50"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-7xl font-bold transform transition duration-500 hover:scale-110">Recrutement</h1>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
